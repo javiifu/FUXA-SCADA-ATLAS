9ace3fb0fc7f70252c589174cb626c12907b49cc
Yago <yagocelma2003@gmail.com>
2026-03-13 09:05:13 +0100

    Actualizacion base de datos

--- Proyecto FUXA/Services/ServicioProductividad.cs
+++ Proyecto FUXA/Services/ServicioProductividad.cs
@@ -6,7 +6,7 @@ namespace Proyecto_FUXA.Services
     //Para calcular la productividad, tomaremos el numero de cilocs reales registrdos y se dividirá entre el objtivo de estos ciclos por hora. se multiplica x100 para botener el porcentaje
     public class ServicioProductividad
     {
-        public decimal CalculateHourlyProductivity(Machine machine, int realCycles)
+        public decimal CalculateHourlyProductivity(Maquina machine, int realCycles)
         {
             if (machine.CiclosHoraObjetivo <= 0) return 0;
             return Math.Round((decimal)realCycles / machine.CiclosHoraObjetivo * 100, 2); //Aquí redondearemos en dos decimales, para ser más claros con el resultado. 
